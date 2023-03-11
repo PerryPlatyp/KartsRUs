ae357c555461e7f4bdd8ad77bfebdc8a9e4f697a
sam <marionette1690@gmail.com>
2023-03-11 01:28:13 -0500

    TBH I don't even know what this change was so ¯\_(ツ)_/¯

--- pages/components/ItemCarousel.js
+++ pages/components/ItemCarousel.js
@@ -1,7 +1,47 @@
 import { Carousel, Container, Row, Col, Button } from 'react-bootstrap';
 import Image from 'next/image';
+import * as PropTypes from "prop-types";
 
 const placeholderImage = '/../public/placeholder.png';
+
+function CarouselComponent(props) {
+    return <Carousel
+        interval={3000}
+        pause={false}
+        indicators={false}
+        controls={false}
+        wrap={true}
+        slide={true}
+    >
+        <Carousel.Item>
+            <div className="d-flex justify-content-center">
+                <Image src={placeholderImage} alt="placeholder image" width={600} height={400} style={props.style}/>
+            </div>
+        </Carousel.Item>
+        <Carousel.Item>
+            <div className="d-flex justify-content-center">
+                <Image src={placeholderImage} alt="placeholder image" width={600} height={400} style={props.style}/>
+            </div>
+        </Carousel.Item>
+        <Carousel.Item>
+            <div className="d-flex justify-content-center">
+                <Image src={placeholderImage} alt="placeholder image" width={600} height={400} style={props.style}/>
+            </div>
+        </Carousel.Item>
+        <Carousel.Item>
+            <div className="d-flex justify-content-center">
+                <Image src={placeholderImage} alt="placeholder image" width={600} height={400} style={props.style}/>
+            </div>
+        </Carousel.Item>
+        <Carousel.Item>
+            <div className="d-flex justify-content-center">
+                <Image src={placeholderImage} alt="placeholder image" width={600} height={400} style={props.style}/>
+            </div>
+        </Carousel.Item>
+    </Carousel>;
+}
+
+CarouselComponent.propTypes = {style: PropTypes.shape({objectFit: PropTypes.string})};
 
 function ItemCarousel() {
     const imageStyle = {
@@ -12,40 +52,7 @@ function ItemCarousel() {
             <Container className="mt-5">
                 <Row className="py-5">
                     <Col>
-                        <Carousel
-                            interval={3000}
-                            pause={false}
-                            indicators={false}
-                            controls={false}
-                            wrap={true}
-                            slide={true}
-                        >
-                            <Carousel.Item>
-                                <div className="d-flex justify-content-center">
-                                    <Image src={placeholderImage} alt="placeholder image" width={600} height={400} style={imageStyle}/>
-                                </div>
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <div className="d-flex justify-content-center">
-                                    <Image src={placeholderImage} alt="placeholder image" width={600} height={400} style={imageStyle} />
-                                </div>
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <div className="d-flex justify-content-center">
-                                    <Image src={placeholderImage} alt="placeholder image" width={600} height={400} style={imageStyle}/>
-                                </div>
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <div className="d-flex justify-content-center">
-                                    <Image src={placeholderImage} alt="placeholder image" width={600} height={400} style={imageStyle}/>
-                                </div>
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <div className="d-flex justify-content-center">
-                                    <Image src={placeholderImage} alt="placeholder image" width={600} height={400} style={imageStyle}/>
-                                </div>
-                            </Carousel.Item>
-                        </Carousel>
+                        <CarouselComponent style={imageStyle}/>
                         <div className="text-center my-3">
                             <h2>Check out our newest collection</h2>
                             <Button variant="primary">See more</Button>
